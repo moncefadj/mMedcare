87f7e8d3cef7805f3783f3e8bd4d84c3e335f598
moncefadj <moncefaouadj2001@gmail.com>
2021-05-25 16:06:13 +0100

    testForGit activity

---
+++ app/src/main/java/com/moncefadj/testgithub/testForGit.java
@@ -0,0 +1,14 @@
+package com.moncefadj.testgithub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class testForGit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
